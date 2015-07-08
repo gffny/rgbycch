1780b6c917313378bab4971571e74dc6209179bf
gffny <gaffney.ie@gmail.com>
2015-07-08 18:40:34 -0400

    updated the application to use an XIB file for the field representation view

---
+++ ios/coachapp/coachapp/coachapp/FieldView.swift
@@ -0,0 +1,63 @@
+//
+//  FieldView.swift
+//  coachapp
+//
+//  Created by John D. Gaffney on 7/8/15.
+//  Copyright (c) 2015 gffny.com. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class FieldView: UIView {
+
+    // Our custom view from the XIB file
+    var view: UIView!
+
+    @IBOutlet weak var fieldName: UILabel!
+
+//    @IBAction func buttonPressed(sender: AnyObject) {
+//        // do something
+//    }
+
+    override init(frame: CGRect) {
+        // 1. setup any properties here
+
+        // 2. call super.init(frame:)
+        super.init(frame: frame)
+
+        // 3. Setup view from .xib file
+        xibSetup()
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        // 1. setup any properties here
+
+        // 2. call super.init(coder:)
+        super.init(coder: aDecoder)
+
+        // 3. Setup view from .xib file
+        xibSetup()
+    }
+
+    func xibSetup() {
+        view = loadViewFromNib()
+
+        // use bounds not frame or it'll be offset
+        view.frame = bounds
+
+        // Make the view stretch with containing view
+        view.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
+
+        // Adding custom subview on top of our view (over any custom drawing > see note below)
+        addSubview(view)
+    }
+
+    func loadViewFromNib() -> UIView {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "FieldRepresentation", bundle: bundle)
+
+        // Assumes UIView is top level and only object in CustomView.xib file
+        let view = nib.instantiateWithOwner(self, options: nil)[0] as UIView
+        return view
+    }
+}
--- ios/coachapp/coachapp/coachapp/GameRecordViewController.swift
+++ ios/coachapp/coachapp/coachapp/GameRecordViewController.swift
@@ -1,5 +1,5 @@
 //
-//  DetailViewController.swift
+//  GameRecordViewController.swift
 //  coachapp
 //
 //  Created by John D. Gaffney on 7/7/15.
@@ -9,67 +9,15 @@
 import UIKit
 
 class GameRecordViewController: UIViewController {
-    
-    weak var kickOffView : UIView!
-    weak var imageView : UIImageView!
 
-    override func viewDidLayoutSubviews() {
-        loadFieldRepresentationView()
-    }
-    
-    func loadFieldRepresentationView() {
-
-        if (kickOffView == nil && imageView == nil) {
-        
-            var screenBounds = UIScreen.mainScreen().bounds
-            var image: UIImage = UIImage(named: "field-representation")!
-            var xPos: CGFloat = screenBounds.width - image.size.width - 10
-            var frame: CGRect = CGRect(origin: CGPoint(x: xPos, y: 28), size: CGSize(width: image.size.width, height: image.size.height))
-        
-            // the field image
-            var imageViewHolder: UIImageView = UIImageView(image: image)
-            imageView = imageViewHolder
-            imageView.frame = frame
-            // the kickoff button view
-            var kickOffViewHolder: UIView = UIView(frame: frame)
-            kickOffView = kickOffViewHolder
-            kickOffView.alpha = 0.6;
-            kickOffView.backgroundColor = UIColor.blackColor();
-            kickOffView.frame = frame;
-            // the kickoff label
-            var button: UIButton = UIButton(frame: CGRectMake(200, 200, 315, 140))
-            button.backgroundColor=UIColor.whiteColor()
-            button.setTitle("Kick Off", forState: UIControlState.Normal)
-            button.setTitleColor(UIColor.blackColor(), forState:UIControlState.Normal)
-            button.addTarget(self, action: "kickOff:", forControlEvents: UIControlEvents.TouchUpInside)
-
-            view.addSubview(imageView)
-            kickOffView.addSubview(button)
-            view.addSubview(kickOffView);
-        }
-        //TODO handle orientation change?
-    }
-
-    @IBAction func kickOff(sender: UIButton!) {
-        var alertView = UIAlertView();
-        alertView.addButtonWithTitle("Ok");
-        alertView.title = "title";
-        alertView.message = "message";
-        alertView.show();
-
-        let subViews = view.subviews
-        for subview in subViews{
-
-            if(subview as UIView == kickOffView) {
-                subview.removeFromSuperview()
-            }
-        }
-    }
+    @IBOutlet weak var fieldView: FieldView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.view.backgroundColor = UIColor.blueColor()
+        
+        fieldView.fieldName.text = "Blah"
+        
     }
+
 }
 
